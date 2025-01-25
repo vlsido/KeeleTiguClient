a64554d0f8cc168a74e221bef990847bf7c5ac31
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-01-25 02:42:25 +0200

    build: update deps

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,19 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [["module:@preact/signals-react-transform"]],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: [
+            {
+              "@preact/signals-react": "@preact-signals/safe-react",
+            },
+          ],
+        },
+      ],
+      "module:@preact-signals/safe-react/babel",
+    ],
     env: {
       production: {
         plugins: ["transform-remove-console"],
