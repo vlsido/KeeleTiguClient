63d08468e72069ac73b37e8ee983ea56660dc0bc
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-01-29 03:58:23 +0200

    perf: create animated TextInput component only once

--- components/util/AnimatedComponentsUtil.ts
+++ components/util/AnimatedComponentsUtil.ts
@@ -1,4 +1,8 @@
-import { Pressable } from "react-native";
+import {
+  Pressable,
+  TextInput
+} from "react-native";
 import Animated from "react-native-reanimated";
 
 export const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+export const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
