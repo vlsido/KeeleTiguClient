b390b7c32e603e27f54500eda3bc6a8ab93d82d7
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-02-07 11:37:19 +0200

    perf: split word data into separate components

---
+++ components/text_components/Usage.tsx
@@ -0,0 +1,44 @@
+import {
+  StyleSheet,
+  View
+} from "react-native";
+import Definitions from "./Definitions";
+import Examples from "../screens/dictionary/Examples";
+
+interface UsageProps {
+  searchString: string | undefined,
+  definitionData: {
+    definitionText: string | undefined;
+    fieldsOfKnowledge: string[];
+    russianTranslations: string[];
+  }[];
+  examples: {
+    estonianExample: string;
+    russianTranslations: string[];
+  }[] | undefined;
+  index: number;
+}
+
+function Usage(props: UsageProps) {
+
+  return (
+    <View style={styles.container}>
+      <Definitions
+        definitionData={props.definitionData}
+        usageIndex={props.index}
+        searchString={props.searchString} />
+      <Examples
+        examples={props.examples}
+        searchString={props.searchString}
+      />
+    </View>
+  )
+}
+
+export default Usage;
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 5,
+  },
+})
