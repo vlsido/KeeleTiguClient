5ab36eac87887b0ce37b4986256290d6ab6e34db
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-02-07 11:42:34 +0200

    chore: add comment

--- components/util/CloudFunctions.ts
+++ components/util/CloudFunctions.ts
@@ -2,7 +2,12 @@ import { httpsCallable } from "firebase/functions";
 import { functions } from "./FirebaseConfig";
 import { OperationError } from "../errors/OperationError";
 
-// connectFunctionsEmulator(functions, "127.0.0.1", 5001);
+// UNCOMMENT TO DEBUG
+// connectFunctionsEmulator(
+//   functions,
+//   "127.0.0.1",
+//   5001
+// );
 
 export async function callCloudFunction(
   name: string,
