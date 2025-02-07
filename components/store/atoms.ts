e1e8ad21293f986ae43f0dbee1eb9f9da4b5c968
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-02-07 11:42:19 +0200

    feat: add atomWithListener

--- components/store/atoms.ts
+++ components/store/atoms.ts
@@ -1,4 +1,5 @@
-import { WritableAtom, atom } from "jotai"
+import { Getter, SetStateAction, Setter, WritableAtom, atom, useSetAtom } from "jotai"
+import { useEffect } from "react"
 
 export function atomWithToggle(initialValue?: boolean,): WritableAtom<boolean, [boolean?], void> {
   const anAtom = atom(
@@ -15,4 +16,67 @@ export function atomWithToggle(initialValue?: boolean,): WritableAtom<boolean, [
   )
 
   return anAtom as WritableAtom<boolean, [boolean?], void>
+}
+
+type Callback<Value> = (
+  get: Getter,
+  set: Setter,
+  newVal: Value,
+  prevVal: Value,
+) => void
+
+export function atomWithListeners<Value>(initialValue: Value) {
+  const baseAtom = atom(initialValue)
+  const listenersAtom = atom<Callback<Value>[]>([])
+  const anAtom = atom(
+    (get) => get(baseAtom),
+    (
+      get, set, arg: SetStateAction<Value>
+    ) => {
+      const prevVal = get(baseAtom)
+      set(
+        baseAtom,
+        arg
+      )
+      const newVal = get(baseAtom)
+      get(listenersAtom).forEach((callback) => {
+        callback(
+          get,
+          set,
+          newVal,
+          prevVal
+        )
+      })
+    },
+  )
+  const useListener = (callback: Callback<Value>) => {
+    const setListeners = useSetAtom(listenersAtom)
+    useEffect(
+      () => {
+        setListeners((prev) => [
+          ...prev,
+          callback
+        ])
+        return () =>
+          setListeners((prev) => {
+            const index = prev.indexOf(callback)
+            return [
+              ...prev.slice(
+                0,
+                index
+              ),
+              ...prev.slice(index + 1)
+            ]
+          })
+      },
+      [
+        setListeners,
+        callback
+      ]
+    )
+  }
+  return [
+    anAtom,
+    useListener
+  ] as const
 }
