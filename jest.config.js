d04551f223df966b19f69c496e0cb65ca1c85e9d
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-02-10 00:03:10 +0200

    test: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,23 @@
 module.exports = {
   preset: "jest-expo",
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/jest.setup.js",
+    "!**/dist/**",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  coverageDirectory: "coverage",
   testEnvironment: "jsdom",
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-redux|firebase|@firebase/.*)",
