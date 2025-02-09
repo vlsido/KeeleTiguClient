932a6c416f270efa307a05e2d65a1e9183a9384a
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-02-09 05:21:09 +0200

    test: add unit tests for some components

---
+++ components/__tests__/components/screens/dictionary/Examples.tsx
@@ -0,0 +1,28 @@
+import { renderWithProviders, screen } from "../../../../../utils/test-utils";
+import Examples from "../../../../screens/dictionary/Examples";
+
+describe(
+  "Examples",
+  () => {
+    test(
+      "renders correctly",
+      () => {
+        const examples = [
+          {
+            estonianExample: "tugev kui karu",
+            russianTranslations: [
+              "силён, как медведь"
+            ]
+          }
+        ];
+
+        renderWithProviders(<Examples
+          examples={examples}
+          searchString="karu"
+        />);
+
+        expect(screen.getByTestId("EXAMPLES.CONTAINER:VIEW")).toBeOnTheScreen();
+      }
+    )
+  }
+);
