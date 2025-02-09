69d2095ce3ebe5e33ca5cbe48cbb61336a441810
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-02-10 00:02:49 +0200

    test: add atoms provider for tests

--- utils/test-utils.tsx
+++ utils/test-utils.tsx
@@ -4,6 +4,8 @@ import {
   RenderOptions
 } from '@testing-library/react-native'
 import { Provider } from 'react-redux'
+import { Provider as JotaiProvider } from "jotai";
+import { useHydrateAtoms } from "jotai/utils";
 import {
   AppStore,
   RootState,
@@ -13,6 +15,20 @@ import {
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
   preloadedState?: Partial<RootState>
   store?: AppStore
+  atomsState?: unknown
+}
+
+function HydrateAtoms({ initialValues, children }) {
+  useHydrateAtoms(initialValues);
+  return children;
+}
+
+function AtomsProvider({ initialValues, children }) {
+  return (
+    <JotaiProvider>
+      <HydrateAtoms initialValues={initialValues}>{children}</HydrateAtoms>
+    </JotaiProvider>
+  )
 }
 
 export function renderWithProviders(
@@ -21,12 +37,19 @@ export function renderWithProviders(
     preloadedState = {},
     // Automatically create a store instance if no store was passed in
     store = setupStore(preloadedState),
+    atomsState = [],
     ...renderOptions
   }: ExtendedRenderOptions = {}
 ) {
 
   function Wrapper({ children }: { children: React.ReactNode }): JSX.Element {
-    return <Provider store={store}>{children}</Provider>
+    return (
+      <Provider store={store}>
+        <AtomsProvider initialValues={atomsState}>
+          {children}
+        </AtomsProvider>
+      </Provider>
+    )
   }
 
   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
