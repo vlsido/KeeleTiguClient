9f0ae417fc8cf7720383e4f1f1da48d9c25a2e83
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-02-19 01:20:37 +0200

    perf: move context to separate folder

---
+++ components/contexts/AuthContext.tsx
@@ -0,0 +1,90 @@
+import React, {
+  createContext,
+  useEffect
+} from "react";
+import {
+  onAuthStateChanged,
+  signInAnonymously,
+  updateProfile
+} from "firebase/auth";
+import {
+  auth,
+  firestore
+} from "../util/FirebaseConfig";
+import {
+  doc,
+  getDoc
+} from "firebase/firestore";
+
+interface AuthContextProps {
+}
+
+export const AuthContext = createContext<AuthContextProps>({
+});
+
+
+function AuthContextProvider({ children }: { children: React.ReactNode }) {
+  useEffect(
+    () => {
+      const unsubscribe = () => {
+        onAuthStateChanged(
+          auth,
+          async (user) => {
+            try {
+              if (user) {
+                if (user.isAnonymous) {
+                } else if (!user.isAnonymous) {
+                  if (user.displayName == null) {
+                    await syncNicknameWithDB(user.uid);
+                  }
+                }
+              } else {
+                await signInAnonymously(auth);
+              }
+            } catch (error) {
+              console.error("auth/error", error);
+            }
+          }
+        );
+      };
+
+      return unsubscribe;
+
+    },
+    []
+  );
+
+  async function syncNicknameWithDB(uid: string) {
+    const userRef = doc(
+      firestore,
+      "users",
+      uid
+    );
+    const userSnap = await getDoc(userRef);
+
+    if (userSnap.exists() && auth.currentUser != null) {
+      await updateProfile(
+        auth.currentUser,
+        { displayName: userSnap.data().nickname }
+      ).then(() => {
+        console.log(
+          "Nickname set to",
+          userSnap.data().nickname
+        );
+      }).catch((error) => {
+        console.error(
+          "Failed to set nickname",
+          error
+        );
+      });
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{}}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContextProvider;
---
+++ components/contexts/ConfigContext.tsx
@@ -0,0 +1,135 @@
+import {
+  RemoteConfig,
+  fetchAndActivate,
+  getRemoteConfig,
+  getValue
+} from "firebase/remote-config";
+import {
+  createContext,
+  useEffect
+} from "react";
+import { app } from "../util/FirebaseConfig";
+import { i18n } from "../store/i18n";
+import ee from "../store/translations/ee.json"
+import { useAppDispatch } from "../../hooks/storeHooks";
+import { clearDictionary } from "../store/slices/dictionarySlice";
+import {
+  atom,
+  useSetAtom
+} from "jotai";
+
+interface ConfigContextProps {
+  remoteConfig: RemoteConfig | null;
+}
+
+export const ConfigContext = createContext<ConfigContextProps>({
+  remoteConfig: null,
+});
+
+export const isUnderMaintenanceAtom = atom<boolean>(false);
+
+function ConfigContextProvider({ children }: { children: React.ReactNode }) {
+  const remoteConfig = getRemoteConfig(app);
+
+  const dispatch = useAppDispatch();
+
+  const setIsUnderMaintenance = useSetAtom(isUnderMaintenanceAtom);
+
+  // if (__DEV__) {
+  //   useEffect(
+  //     () => {
+  //       const unsubscribe = i18n.onChange(() => {
+  //         console.log("I18n has changed!");
+  //       });
+  //
+  //       return unsubscribe;
+  //     },
+  //     []
+  //   );
+  // }
+
+  useEffect(
+    () => {
+      remoteConfig.settings.minimumFetchIntervalMillis = 600000; // 600000ms = 10 minutes
+
+      remoteConfig.defaultConfig = {
+        last_cache_invalidation_timestamp: 1739391134,
+        is_under_maintenance: false,
+        maintenance_text: "Uuendame appi, proovige uuesti hiljem!"
+      };
+
+      fetchAndActivate(remoteConfig).
+        then(() => {
+
+          const lastCacheInvalidationTimestamp = getValue(
+            remoteConfig,
+            "last_cache_invalidation_timestamp"
+          ).asString();
+
+          const cachedLastCacheInvalidationTimestamp = localStorage.getItem("last_cache_invalidation_timestamp");
+
+          if (cachedLastCacheInvalidationTimestamp == null || lastCacheInvalidationTimestamp !== cachedLastCacheInvalidationTimestamp) {
+            localStorage.setItem(
+              "last_cache_invalidation_timestamp",
+              lastCacheInvalidationTimestamp
+            );
+            removeCache();
+            loadTranslations("ee");
+
+            return;
+          }
+
+          setIsUnderMaintenance(getValue(
+            remoteConfig,
+            "is_under_maintenance"
+          ).asBoolean());
+
+          localStorage.setItem(
+            "maintenanceText",
+            getValue(
+              remoteConfig,
+              "maintenance_text"
+            ).asString()
+          );
+
+          dispatch({ type: "dictionary/loadCachedWords" });
+
+          loadTranslations("ee");
+        }).catch((error) => {
+          console.error(
+            "error fetching config",
+            error
+          );
+          loadTranslations("ee");
+        });
+
+    },
+    []
+  );
+
+  function removeCache() {
+    localStorage.removeItem("myDictionary");
+    localStorage.removeItem("allWords");
+    localStorage.removeItem("wordsAndExamData");
+    dispatch(clearDictionary());
+  }
+
+  function loadTranslations(locale: string) {
+    i18n.defaultLocale = locale;
+    i18n.locale = locale;
+
+    i18n.store(ee);
+  }
+
+  const value = {
+    remoteConfig,
+  }
+
+  return (
+    <ConfigContext.Provider value={value}>
+      {children}
+    </ConfigContext.Provider>
+  );
+}
+
+export default ConfigContextProvider;
