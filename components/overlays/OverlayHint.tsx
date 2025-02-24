14203097c36a03e8d3658f6bbdb00bf9c665ce8e
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-02-24 15:24:34 +0200

    feat: add overlayhint

---
+++ components/overlays/OverlayHint.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, View } from "react-native";
+import Animated, {
+  FadeIn,
+  FadeOut,
+  ReduceMotion,
+} from "react-native-reanimated";
+import { AnimatedPressable } from "../util/AnimatedComponentsUtil";
+
+interface OverlayHintProps {
+  isVisible: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+function OverlayHint(props: OverlayHintProps) {
+  if (props.isVisible === false) {
+    return null;
+  }
+
+  return (
+    <AnimatedPressable
+      style={[styles.backgroundContainer]}
+      onPress={props.onClose}
+      entering={FadeIn
+        .reduceMotion(ReduceMotion.System)
+        .duration(133)
+      }
+      exiting={FadeOut
+        .reduceMotion(ReduceMotion.System)
+        .duration(133)
+      }
+    >
+      {props.children}
+    </AnimatedPressable>
+  );
+}
+
+export default OverlayHint;
+
+const styles = StyleSheet.create({
+  backgroundContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    backgroundColor: "rgba(0, 0, 0, 0.85)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
