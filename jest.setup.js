340557a57c1d9117adbd6d1163d3debfb8c869fe
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-02-28 23:14:09 +0200

    test: add mocks to jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -2,7 +2,25 @@ import "react-native-gesture-handler/jestSetup";
 
 import { setUpTests as reanimatedSetUpTests } from "react-native-reanimated";
 
+import { TextEncoder, TextDecoder } from "util";
+import { ReadableStream } from "stream/web";
+
+Object.assign(global, { TextDecoder, TextEncoder, ReadableStream });
+global.setImmediate =
+  global.setImmediate || ((fn, ...args) => global.setTimeout(fn, 0, ...args));
 reanimatedSetUpTests();
+
+jest.mock("expo-font", () => ({
+  isLoaded: jest.fn(),
+  forEach: jest.fn(),
+  loadAsync: jest.fn(),
+}));
+
+jest.mock("@expo/vector-icons", () => ({
+  Ionicons: "Ionicons",
+  MaterialIcons: "MaterialIcons",
+  Icon: "Icon",
+}));
 
 jest.mock("firebase/functions", () => ({
   httpsCallable: jest.fn(() => Promise.resolve({ data: {} })),
