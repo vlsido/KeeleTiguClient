6eb409314c03128e41fc6c33b6fe69adfabc42e5
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-02-28 19:08:06 +0200

    test: fix tests

--- __tests__/unit/app/dictionary.test.tsx
+++ __tests__/unit/app/dictionary.test.tsx
@@ -1,14 +1,31 @@
 import Dictionary from "../../../app/(tabs)/dictionary";
-import { renderWithProviders, screen } from "../../../utils/test-utils";
 import { DictionaryState } from "../../../components/store/slices/dictionarySlice";
-import { renderRouter } from "expo-router/testing-library"
-import RootLayout from "../../../app/_layout";
+import { renderRouter, screen } from "expo-router/testing-library"
 import RootLayoutTabs from "../../../app/(tabs)/_layout";
+import { Provider } from "react-redux";
+import { setupStore } from "../../../components/store/store";
 
+import { Provider as JotaiProvider } from "jotai";
+import { View } from "react-native";
+import { useHydrateAtoms } from "jotai/utils";
 
+function HydrateAtoms({ initialValues, children }) {
+  useHydrateAtoms(initialValues);
+  return children;
+}
+
+function AtomsProvider({ initialValues, children }) {
+  return (
+    <JotaiProvider>
+      <HydrateAtoms initialValues={initialValues}>{children}</HydrateAtoms>
+    </JotaiProvider>
+  )
+}
 describe(
   "Dictionary",
   () => {
+
+    const MockComponent = jest.fn(() => <View />);
     test(
       "renders 'no words in dictionary' text when myDictionary length is 0",
       () => {
@@ -17,26 +34,34 @@ describe(
           examDictionary: [],
           words: []
         };
-        renderRouter({
-          _layout: () => <RootLayoutTabs />,
-          dictionary: () => <Dictionary />
-        }, {
-          initialUrl: "/dictionary"
+
+        const store = setupStore({
+          settings: { language: "ee", highlightRussianAccentLetters: true },
+          dictionary: initialDictionary
         });
-        // renderWithProviders(
-        //   <Dictionary />,
-        //   {
-        //     preloadedState: {
-        //       dictionary: initialDictionary
-        //     }
-        //   }
-        // );
+
+        renderRouter(
+          {
+            _layout: () => (
+              <Provider store={store}>
+                <AtomsProvider initialValues={[]} >
+                  <RootLayoutTabs />
+                </AtomsProvider>
+              </Provider>),
+            "dictionary": () => <Dictionary />,
+            "index": MockComponent,
+            "search": MockComponent
+          },
+          { initialUrl: "/dictionary" }
+        );
+
+
 
         expect(screen.getByTestId("DICTIONARY.WORDS_EMPTY:VIEW")).toBeOnTheScreen();
         expect(screen.getByTestId("DICTIONARY.WORDS_EMPTY:TEXT")).toBeOnTheScreen();
       }
     );
-
+    //
     test(
       "renders words list when myDictionary not empty",
       () => {
@@ -66,14 +91,23 @@ describe(
           words: []
         };
 
-        renderWithProviders(
-          <Dictionary />,
+        const store = setupStore({ dictionary: initialDictionary });
+        renderRouter(
           {
-            preloadedState: {
-              dictionary: initialDictionary
-            }
-          }
+            _layout: () => (
+              <Provider store={store}>
+                <AtomsProvider initialValues={[]} >
+                  <RootLayoutTabs />
+                </AtomsProvider>
+              </Provider>),
+            "dictionary": () => <Dictionary />,
+            "index": MockComponent,
+            "search": MockComponent
+          },
+          { initialUrl: "/dictionary" }
         );
+
+
 
         expect(screen.getByTestId("DICTIONARY.CONTAINER:VIEW")).toBeOnTheScreen();
         expect(screen.getByTestId("DICTIONARY.WORD_COUNT:TEXT")).toBeOnTheScreen();
