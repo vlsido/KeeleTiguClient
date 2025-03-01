b94da1539d56e880ea7364726b8be8884e6db8db
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-03-01 12:40:45 +0200

    perf: get focused tab at app load

--- components/StackHeader.tsx
+++ components/StackHeader.tsx
@@ -8,15 +8,25 @@ import { i18n } from "./store/i18n";
 import SettingsButton from "./settings/SettingsButton";
 import CustomIconButton from "./buttons/CustomIconButton";
 import { ArrowBackIcon } from "./icons/ArrowBackIcon";
+import { useCallback } from "react";
 
 function StackHeader() {
+
+  const goToMainPage = useCallback(() => {
+    if (router.canGoBack()) {
+      return router.back();
+    }
+
+    router.dismissAll();
+    return router.replace("/");
+  }, [router]);
 
   return (
     <View style={styles.container}>
       <View style={styles.buttonsContainer}>
         <CustomIconButton
           testID="STACK_HEADER.BACK_ICON:PRESSABLE"
-          onPress={() => router.replace("/")}
+          onPress={goToMainPage}
           aria-label={i18n.t("StackHeader_go_back", { defaultValue: "Tagasi" })}
         >
           <ArrowBackIcon />
